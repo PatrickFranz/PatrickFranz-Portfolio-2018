64a1578a546a2d9df797b02d5a8b184fb3a2ec52
Patrick Franz <clymbing@gmail.com>
2017-07-04 22:33:37 -0700

    Added 'return to top' button.

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,25 +5,32 @@
   const projectsHead = document.getElementById('port--project-sec');
   const skillsHead   = document.getElementById('port--skills-sec');
   const contactHead  = document.getElementById('port--contact-sec');
+  const returnTopButton = document.getElementById('returnTop');
+
+  window.addEventListener('scroll', function(event){
+    if(window.pageYOffset > 200){
+      returnTopButton.style.display = 'block';
+    } else {
+      returnTopButton.style.display = 'none';
+    }
+  });
 
   menuItems.forEach(function(item){
     item.addEventListener('click', function(e){
       switch(item.dataset.target){
+
         case aboutHead.id:{
           scrollWindowTo(aboutHead.offsetTop);
-         
           break;
         }
         case projectsHead.id: {
           scrollWindowTo(projectsHead.offsetTop);
           break;
         }
-
         case skillsHead.id: {
           scrollWindowTo(skillsHead.offsetTop);
           break;
         }
-
         case contactHead.id: {
          scrollWindowTo(contactHead.offsetTop);
           break;
@@ -35,7 +42,7 @@
 
 function scrollWindowTo(y){
   const SCROLL_SPEED = 50;
-  const THRESHOLD = 20;
+  const THRESHOLD = 30; //Small values can cause a jittering.
   let currentLoc = window.pageYOffset;
   if(Math.abs(y - currentLoc) > THRESHOLD){
     var direction = y - currentLoc < 1 ? -1 : 1;
