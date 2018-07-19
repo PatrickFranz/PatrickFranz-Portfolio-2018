7a3120dca8554c9370bd8caee7663f823e81e659
Patrick Franz <clymbing@gmail.com>
2018-07-18 23:15:26 -0700

    Carousel elements now scroll properly. Still need to add animation.

--- js/carousel.js
+++ js/carousel.js
@@ -2,25 +2,32 @@ const projectList = Array.from(document.querySelectorAll('.project-item'));
 const scrollButtons = document.querySelectorAll('.scroll');
 const SHOW_ELEMENTS = 3;
 let pageNumber = 1;
-
 scrollButtons.forEach(btn => btn.addEventListener('click', handleScroll));
 
-projectList.forEach((project, index)=> {
-  if(index < (SHOW_ELEMENTS * pageNumber)){
-    project.style.display = "block"
-  } else {
-    project.style.display = "none"
-  }
-})
+showProjects();
+
+function showProjects(){
+  projectList.forEach((project, index)=> {
+    if(index < (SHOW_ELEMENTS * pageNumber) 
+        && index > (SHOW_ELEMENTS * pageNumber) - SHOW_ELEMENTS -1){
+          project.classList.remove('hide')
+    } else {
+          project.classList.add('hide');
+    }
+  });
+}
 
 function handleScroll(e){
   const pagesRequired = Math.ceil(projectList.length / SHOW_ELEMENTS);
-  
-  console.log(pagesRequired)
   if(this.dataset.dir === 'right'){
-    pageNumber < pagesRequired && pageNumber++
+    if(pageNumber < pagesRequired){
+      pageNumber++;
+    }
   } else {
-    pageNumber > 1 && pageNumber--;
+    if(pageNumber > 1){
+      pageNumber--;
+    }
   }
-  console.log('current page: ', pageNumber )
+  console.log('pageNumber: ', pageNumber)
+  showProjects();
 }
